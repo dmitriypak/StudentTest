77c928d25ece23a8e03756a8961837e33a173968
Dmitriy Pak <fly2sky7777@mail.ru>
2019-07-10 23:04:52 +0300

    initial commit

---
+++ src/main/java/ru/projects/edu/spring/task3/StudentTestApplication.java
@@ -0,0 +1,13 @@
+package ru.projects.edu.spring.task3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentTestApplication.class, args);
+	}
+
+}
